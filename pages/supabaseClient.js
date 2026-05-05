5eb1d72b97f74784a50a8be201003f0d380653d9
IM23a-buerkim <buerkim@bzz.ch>
2026-05-05 11:11:26 +0200

    Projekt test

--- pages/supabaseClient.js
+++ pages/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
+
+const SUPABASE_URL = 'https://lirupsblqmigmemjymzr.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_NvqlTXE9lTHQj_d6VwV3Xg_6HGVOaaH';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
